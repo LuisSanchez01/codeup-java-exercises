6d9869cf2f965f673cc3dc1beb0bf34489b85aa6
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-03-24 16:28:25 -0500

    add YoHey.java and HelloWorld.java

---
+++ src/YoHey.java
@@ -0,0 +1,69 @@
+public class YoHey {
+    public static void main(String[] args) {
+        System.out.println("Yo, hey!");
+
+        int myFavoriteNumber = 867;
+
+        System.out.println("myFavoriteNumber = " + myFavoriteNumber);
+
+
+        String myString = "Go Niners!";
+
+        System.out.println("myString = " + myString);
+
+
+        myString = "Go 49ers!";
+
+        System.out.println("myString = " + myString);
+
+//        myString = 3.14159;
+
+        System.out.println("myString = " + myString);
+
+        long myNumber = 123;
+
+        System.out.println("myNumber = " + myNumber);
+
+
+//        myFloat = (float) myNumber;
+//
+//        System.out.println("myNumber = " + myNumber);
+
+//        int x = 5;
+//        System.out.println(x++); It is post so it adds afterwards, displays as 5
+//        System.out.println(x); adds one, displays as 6
+
+//        int x = 5;
+//        System.out.println(++x); adds pre display, displays as 6
+//        System.out.println(x); adds one, displays as 6
+
+//          var class = ; cannot resolve a var
+
+//        String theNumberThree = "three";    ClassCastException, String cannot be cast
+//        Object o = theNumberThree;          to class java.lang.Integer.
+//        int three = (int) o;
+
+//        int three = (int) "three";          Incompatible types: java.lang.String cannot
+//                                            be converted to int
+//        System.out.println("three = " + three);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println("x = " + x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println("y = " + y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+        System.out.println("y = " + y);
+
+//        Integer.MAX_VALUE
+
+    }
+}
