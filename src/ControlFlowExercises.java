5344f7cbf2a2d31bb6ba67f286fd5f50ece5633f
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-03-28 16:55:15 -0500

    add Control Statements and Loops exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -7,16 +7,60 @@ public class ControlFlowExercises {
 
 //            Create a do-while loop that will count by 2's starting with 0 and ending at 100.
 //            Follow each number with a new line.
+//            int i = 2;
+//
+//            do {
+//                System.out.println(i * 2);
+//                i++;
+//            } while (i <= 100);
+//                System.out.println("\n");
+//            }
+//        }
 //            Alter your loop to count backwards by 5's from 100 to -10.
+//        int i = 100;
+//
+//        do {
+//            System.out.println(i + " ");
+//            i-=5;
+//        } while (i >= -10);
+//        System.out.println("\n");
+//    }
+//}
 //            Create a do-while loop that starts at 2, and displays the number squared on each
 //            line while the number is less than 1,000,000. Output should equal:
+//           int i = 2;
+//
+//           do {
+//                System.out.println(i*=2);
+//                i++;
+//           } while (i < 1000000);
+//           System.out.println("\n");
+//    }
+//}
 
-            do {
-                System.out.println("You will see this despite the condition!");
-            }
-            while (i < 10) {
-                System.out.println();
-            }
+//        One of the most common interview questions for entry-level programmers is the FizzBuzz test.
+//        Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
+//
+//        Write a program that prints the numbers from 1 to 100.
+//        For multiples of three: print “Fizz” instead of the number.
+
+//        for (int i = 1; i < 100; i++) {
+//            System.out.println(i);
+//        if(i % 3 == 0) {
+//            System.out.println("Fizz");
+//        }
+//    }
+//}}
+
+//        For the multiples of five: print “Buzz”.
+    for (int i = 1; i <= 100; i++) {
+             System.out.println(i);
+        if(i % 5 == 0) {
+             System.out.println("Buzz");
         }
     }
-}
+    }}
+
+//        For numbers which are multiples of both three and five: print “FizzBuzz”.
+
+
