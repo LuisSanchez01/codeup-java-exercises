874115f261a7cad7dd69ed6ca802132b039c6b85
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-03-29 11:42:53 -0500

    add StringExercises.java

---
+++ src/StringExercise.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+
+public class StringExercise {
+    public static void main(String[] args) {
+//        For each of the following output examples: create a String
+//        variable that contains the desired output and print it out to
+//        the console, you can do this with only one String variable and
+//        one print statement for each output example.
+//
+//
+//        String message = "We don't need no education";
+        if ("We don't need no education".equals("We don't need no thought control")) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+
+//        String input = "Check "this" out!, "s inside of "s!";
+
+        if ("In windows, the main drive is usually C:".equals("In windows, the main drive is usually C:")) {
+            System.out.println("Strings are equal");
+        }
+
+//        I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+
+    }
+}
