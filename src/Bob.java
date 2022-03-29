c1dd6188d33acd86d67ef719c817bcda4d6c9599
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-03-29 12:02:46 -0500

    add Bob.java

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+//        Bob is a lackadaisical teenager. In conversation, his responses are very
+//        limited.
+//
+//        Bob answers 'Sure.' if you ask him a question. (the input ends with a
+//        question mark)
+//        He answers 'Whoa, chill out!' if you yell at him. (the input ends with an
+//        exclamation mark)
+//        He says 'Fine. Be that way!' if you address him without actually saying
+//        anything. (empty input)
+//        He answers 'Whatever.' to anything else.
+//        Write the Java code necessary so that a user of your command line
+//        application can have a conversation with Bob.
 
-    }
-}
+        Scanner sc = new Scanner(System.in);
+        String choice = "y";
+        while (choice.equalsIgnoreCase("y")) {
+
+            //  get input from user
+            System.out.println("Enter your question:\t\t");
+            double question = sc.nextDouble();
+
+            // convert user numeric grade into letter grade
+
+
+        }
+    }}
