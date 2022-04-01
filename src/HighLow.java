e641423b820303bd9e80ca6a7c7a84a14d478c84
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-01 14:28:01 -0500

    add HighLow.java completed

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,11 +1,6 @@
 public class HighLow {
 
-    public static void init() {
-        System.out.println("Welcome to the High / Low game");
-        int randomNum = MethodsExercises.getRandomInt(1, 100);
-        System.out.println(randomNum);
-        System.out.println("Guess a number between 1 and 100");
-        int guess = MethodsExercises.getInteger(1, 100);
+    public static void printGuessMessage(int guess, int randomNum) {
         if (guess == randomNum) {
             System.out.println("GOOD GUESS");
         } else if (guess < randomNum){
@@ -13,6 +8,15 @@ public class HighLow {
         } else {
             System.out.println("Lower");
         }
+    }
+
+    public static void init() {
+        System.out.println("Welcome to the High / Low game");
+        int randomNum = MethodsExercises.getRandomInt(1, 100);
+//        System.out.println(randomNum); FOR DEBUGGING
+        System.out.println("Guess a number between 1 and 100");
+        int guess = MethodsExercises.getInteger(1, 100);
+        printGuessMessage(guess, randomNum);
     }
 
 
