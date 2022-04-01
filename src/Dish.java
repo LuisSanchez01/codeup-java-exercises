ddd34186fa5d5883719e1a1414036e773f97c4b6
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-01 15:38:03 -0500

    add Person.java

--- src/Dish.java
+++ src/Dish.java
@@ -1,11 +1,11 @@
 public class Dish {
-//   public static printSummary void {
-//
-//
-//       class Dish() {
-//           private String nameOfDish
-//           private int costInCents
-//           private boolean wouldRecommend
-//       }
-//    }
+
+   public static void printSummary {
+
+           private String nameOfDish
+           private int costInCents
+           private boolean wouldRecommend
+
+       }
+    }
 }
