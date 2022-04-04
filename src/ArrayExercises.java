635f16769a5440399a5bc4dd4db294827ab19763
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-04 12:33:14 -0500

    add example

--- src/ArrayExercises.java
+++ src/ArrayExercises.java
@@ -26,13 +26,67 @@ public class ArrayExercises {
         System.out.println(person[0]);
         System.out.println(person[1]);
         System.out.println(person[2]);
-    } //end of main
 
-    public static Person[] addPerson(Person[] pArray, Person newPerson) {
+//        adding after the static method Person
+        Person[] newBiggerArray = ArrayExercises.addPerson(person, new Person("Jay"));
+//      for (Person persons : newBiggerArray){
+//          person.sayHello();
+        System.out.println(Arrays.toString(newBiggerArray));
+    }
+
+     //end of main
+
+    public static Person[] addPerson(Person[] person, Person newPerson) {
 //declare the new array, one space bigger
+      Person[] newBiggerArray = new Person[person.length + 1];
+      for(int i = 0; i < person.length; i++) {
+          newBiggerArray[i] = person[i];
+      }
+      newBiggerArray[newBiggerArray.length - 1] = newPerson;
+      return newBiggerArray;
+    }
+//    Once this is done, add to main the newPersons array
+
 
     }
 
+//     Javier's work through line 91
 
-    }
+//public class ArraysExercises {
+//    public static Person[] addPerson(Person[] persons, Person n00b){
+//        Person[] newBiggerArray = new Person[persons.length +1];
+//        for(int i = 0; i < persons.length; i++){
+//            newBiggerArray[i] = persons[i];
+//        }
+//        newBiggerArray[newBiggerArray.length -1] = n00b;
+//        return newBiggerArray;
+//    }
+//
+//    public static Person[] addPersonWithCopyOf(Person[] persons, Person n00b){
+//        Person[] newPersonArray;
+//        newPersonArray = Arrays.copyOf(persons, persons.length + 1);
+//        newPersonArray[newPersonArray.length -1] = n00b;
+//        return newPersonArray;
+//    }
+//
+//
+//
+//    public static void main(String[] args) {
+//        int[] numbers = {1, 2, 3, 4, 5};
+////        System.out.println(Arrays.toString(numbers));
+//        Person[] persons = new Person[3];
+//        persons[0] = new Person("Justin");
+//        persons[1] = new Person("Dane");
+//        persons[2] = new Person("Javier");
+//
+////        for (Person person : persons) {
+////            System.out.println(person.getName());
+////        }
+//
+//        Person[] newPersonsArray = ArraysExercises.addPersonWithCopyOf(persons, new Person("Jay"));
+//        for(Person person: newPersonsArray){
+//            person.sayHello();
+//        }
+//        System.out.println(Arrays.toString(newPersonsArray));
+//    }
 
