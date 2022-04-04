86e8ed4bb1aef1ed642092a68aa28e4a7b0eb64b
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-04 13:55:03 -0500

    add Adjective.java

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class ServerNameGenerator {
+    public static void main(String[] args) {
+
+
+        Adjective[] adjectives = new Adjective[10];
+        adjectives[0] = new Adjective("Handsome");
+        adjectives[1] = new Adjective("Intelligent");
+        adjectives[2] = new Adjective("Athletic");
+        adjectives[3] = new Adjective("Charming");
+        adjectives[4] = new Adjective("Gentle");
+        adjectives[5] = new Adjective("Sharp");
+        adjectives[6] = new Adjective("Punctual");
+        adjectives[7] = new Adjective("Attentive");
+        adjectives[8] = new Adjective("Hilarious");
+        adjectives[9] = new Adjective("Helpful");
+
+        System.out.println(adjectives[0]);
+        System.out.println(adjectives[1]);
+        System.out.println(adjectives[2]);
+        System.out.println(adjectives[3]);
+        System.out.println(adjectives[4]);
+        System.out.println(adjectives[5]);
+        System.out.println(adjectives[6]);
+        System.out.println(adjectives[7]);
+        System.out.println(adjectives[8]);
+        System.out.println(adjectives[9]);
+
+//        Only showing memory location but not the name
+
+
+    }
+}
