64773fa2a6b49d60b1c1dc6064e2d1c1b0642d0f
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-05 09:51:19 -0500

    add Shapes directory

---
+++ src/Shapes/Rectangle.java
@@ -0,0 +1,15 @@
+package Shapes;
+
+public class Rectangle {
+    protected length;
+    protected width;
+}
+
+        public int getArea {
+        area = length x width
+        }
+
+
+        public int getPerimeter {
+        perimeter = 2 x length + 2 x width
+        }
---
+++ src/Shapes/ShapesTest.java
@@ -0,0 +1,9 @@
+package Shapes;
+
+public class ShapesTest {
+    public static void main(String[] args) {
+       public void box1(){
+            System.out.println("width:" 4, "length:" 5);
+        }
+    }
+}
