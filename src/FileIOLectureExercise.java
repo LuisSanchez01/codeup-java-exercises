4900038c2e8b9d0b5ad552817baa1952288d9431
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-08 15:02:30 -0500

    add dishes.txt dishes

--- src/FileIOLectureExercise.java
+++ src/FileIOLectureExercise.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FileIOLectureExercise {
@@ -65,6 +68,40 @@ public class FileIOLectureExercise {
 
         FileManager dishesFile = new FileManager("dishes.txt", "data");
         dishesFile.printLines();
+
+
+                fileData.add("additional item");
+                fileData.add("additional item2");
+
+        try {
+            Files.write(dishesPath, fileData);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error creating files " + dishesPath.getFileName());
+        }
+
+        List<String> moreDishes = Arrays.asList("dish 1", "dish 2", "dish 3");
+        System.out.println(moreDishes.toString());
+
+        try {
+            Files.write(dishesPath, moreDishes, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error creating files " + dishesPath.getFileName());
+        }
+
+        try {
+            fileData = Files.readAllLines(dishesPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("File or directory could not be created");
+        }
+
+        for (String line : fileData) {
+            System.out.println(line);
+        }
+
+
 
     }
 }
